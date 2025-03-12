22c5a18b64fed003c77b0213c9c973692a6e22aa
01tintim <159555102+01tintim@users.noreply.github.com>
2025-03-12 12:57:23 +0000

    Perte 2 payment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,10 +1,23 @@
+import {z} from "zod";
+
+const PaymentSchema = z.object({
+    data: z.string().datetime({message: 'Data inválida'}),
+    recibo: z.number({message: 'Recibo inválido'}).min(1,{message: 'Recibo inválido'}),
+    valor: z.number({message: 'Valor invalido'}).min(0,{message: 'Valor inválido'}),
+    observacao: z.string().optional(), 
+});
+
 const PaymentController = {
 
     async createPayment(req, res){
         try {   
             const { data, recibo, valor, observação} = req.body;
+            PaymentSchema.parse({data, recibo, valor, observação});
             return res.status(201).json({message: 'Payment created', dataReq: {data, recibo, valor, observação}});            
         } catch (error) {
+            if (error instanceof z.ZodError) {
+                return res.status(400).json({message: "Validation error", datails: error.errors});
+            }
             return res.status(500).json({message: 'Internal server error'});
         }
     },
